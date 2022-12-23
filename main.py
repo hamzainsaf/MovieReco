d7e1507bf41bb145b5612624c6d3b9312e9fdcf7
hamzainsaf <muhammadhamzainsaf@gmail.com>
2022-12-23 11:33:20 +0100

    update 3

--- main.py
+++ main.py
@@ -24,50 +24,61 @@ feature_vectors = vectorizer.fit_transform(combined_features)
 similarity = cosine_similarity(feature_vectors)
 
 # print(similarity.shape)
-
-
-string_input = input('1 for movie by name \n'
-                     '2 for movie by genre \n'
-                     '3 for movie by cast \n'
-                     '4 for movie by director')
-menu_press = int(string_input)
-
-if menu_press == 1:
-    input1 = input('Enter your Favourite movie')
-elif menu_press == 2:
-    input1 = input('Enter the Genre')
-elif menu_press == 3:
-    input1 = input('Enter the Cast')
-else:
-    input1 = input('Enter name of the Director')
-
 list_of_movies = movies_data['title'].to_list()
+list_of_movies_g = movies_data['genres'].to_list()
+list_of_movies_c = movies_data['cast'].to_list()
+list_of_movies_d = movies_data['director'].to_list()
 
 
-find_close_match = difflib.get_close_matches(input1, list_of_movies)
+def check(column_name, input, list1):
+    find_close_match = difflib.get_close_matches(input, list1)
+
+    close_match = find_close_match[0]
+
+    index_of_movie = movies_data[getattr(movies_data, column_name) == close_match]['index'].values[0]
+    similarity_score = list(enumerate(similarity[index_of_movie]))
+    sorted_simi = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    print('Movies suggested for you: ')
+
+    i = 1
+
+    for movie in sorted_simi:
+        index = movie[0]
+        title_from_index = movies_data[movies_data.index == index]['title'].values[0]
+        if i <= 10:
+            print(i, '.', title_from_index)
+            i += 1
 
 
-close_match = find_close_match[0]
-index_of_movie = movies_data[movies_data.title == close_match]['index'].values[0]
+menu_press = 10
+while menu_press != 0:
+    string_input = input('1 for movie by name \n'
+                         '2 for movie by genre \n'
+                         '3 for movie by cast \n'
+                         '4 for movie by director\n'
+                         '0 for Quit\n'
+                         )
+
+    menu_press = int(string_input)
+
+    if menu_press == 1:
+        c_name = 'title'
+        input1 = input('Enter your Favourite movie \n')
+        check(c_name, input1, list_of_movies)
 
 
-
-similarity_score = list(enumerate(similarity[index_of_movie]))
-
-sorted_simi = sorted(similarity_score, key=lambda x: x[1], reverse=True)
-
-
-
-print('Movies suggested for you: ')
+    elif menu_press == 2:
+        c_name = 'genres'
+        input1 = input('Enter the Genre \n')
+        check(c_name, input1, list_of_movies_g)
 
 
-i = 1
+    elif menu_press == 3:
+        c_name = 'cast'
+        input1 = input('Enter the Cast\n')
+        check(c_name, input1, list_of_movies_c)
 
-for movie in sorted_simi:
-    index = movie[0]
-    title_from_index = movies_data[movies_data.index == index]['title'].values[0]
-    if (i <= 20):
-        print(i, '.', title_from_index)
-        i += 1
-
-
+    elif menu_press == 4:
+        c_name = 'director'
+        input1 = input('Enter name of the Director\n')
+        check(c_name, input1, list_of_movies_d)
